746a63e66f28942c0c7aee0bc64a4c3dd3e46b11
johnsoncodehk <johnsoncodehk@gmail.com>
2023-01-28 20:35:27 +0800

    workflow: test

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import { binarySearch } from "./binarySearch";
 
 const NO_DATA_SYMBOL: unique symbol = Symbol();
 
-export type Segment<T = typeof NO_DATA_SYMBOL> = WithString<
+export type Segment<T = typeof NO_DATA_SYMBOL> = string | (
 	T extends typeof NO_DATA_SYMBOL ?
 	// text, source, source range / offset
 	[string, string | undefined, number | [number, number]] :
 	// text, source, source range / offset, data
 	[string, string | undefined, number | [number, number], T]
->;
-
-type WithString<T> = T | string;
+);
 
 export function getLength(segments: Segment<any>[]) {
 	let length = 0;
